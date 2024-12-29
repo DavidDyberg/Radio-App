375bda94003bf407fe031abd52c880811f4f723e
David Dyberg <david.dyberg@gmail.com>
2024-12-29 18:29:22 +0100

    setup NativeWindCss

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,37 @@
-import { Stack } from "expo-router";
+import { Stack } from "expo-router/stack";
 import React from "react";
+import { Button } from "react-native";
+import "../global.css";
 
 export default function RootLayout() {
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#f4511e",
+        },
+        headerRight: () => (
+          <Button
+            onPress={() => {
+              alert("This is a button!");
+            }}
+            title="Info"
+            color="#fff"
+          />
+        ),
+      }}
+    >
       <Stack.Screen
         name="index"
         options={{
-          headerTitle: "Home",
+          headerTitle: "Radio Kanaler",
+        }}
+      />
+      <Stack.Screen
+        name="settings"
+        options={{
+          headerTitle: "Inställningar",
+          headerBackTitle: "Tillbaka",
         }}
       />
     </Stack>
--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,13 @@
-import { StyleSheet, View, Text } from "react-native";
+import { View, Text } from "react-native";
+import { Link } from "expo-router";
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Home333</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-red-700">Home3333</Text>
+      <Link href="/settings">
+        <Text className="text-blue-800">Settings</Text>
+      </Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
