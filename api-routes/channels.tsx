3d6d539844c3c629e08f194485f869d39fa37995
David Dyberg <david.dyberg@gmail.com>
2025-01-11 18:02:56 +0100

    add paginated API response

--- api-routes/channels.tsx
+++ api-routes/channels.tsx
@@ -1,11 +1,33 @@
-export const fetchChannels = async () => {
-  const ApiUrl = "https://api.sr.se/api/v2/channels/?format=json";
+import { ChannelType, PaginationType } from "@/lib/types";
 
-  const response = await fetch(ApiUrl);
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
+export const fetchChannels = async () => {
+  let allChannels: ChannelType[] = [];
+  let nextPageUrl: string | null =
+    "https://api.sr.se/api/v2/channels/?format=json";
+
+  while (nextPageUrl) {
+    const response = await fetch(nextPageUrl);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data: { channels: ChannelType[]; pagination: PaginationType } =
+      await response.json();
+    allChannels = allChannels.concat(data.channels);
+    nextPageUrl = data.pagination.nextpage || null;
   }
 
-  const data = await response.json();
-  return data.channels;
+  return allChannels;
 };
+
+// export const fetchChannels = async () => {
+//   const ApiUrl = "https://api.sr.se/api/v2/channels/?format=json";
+
+//   const response = await fetch(ApiUrl);
+//   if (!response.ok) {
+//     throw new Error(`HTTP error! status: ${response.status}`);
+//   }
+
+//   const data = await response.json();
+//   return data.channels;
+// };
