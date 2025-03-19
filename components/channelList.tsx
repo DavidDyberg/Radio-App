50cb8e21b734d733d11527c4dfa36e2a378dc01c
David Dyberg <david.dyberg@gmail.com>
2025-03-19 16:53:07 +0100

    feat: add async storage for the app to remember  the selected p4-channel after restart

--- components/channelList.tsx
+++ components/channelList.tsx
@@ -1,13 +1,14 @@
 import type { ChannelType } from "@/lib/types";
 import { View, Text, Image, Pressable } from "react-native";
 import { Audio } from "expo-av";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CustomButton } from "@/components/button";
 import { SizeOption, useSize } from "@/utils/SizeProvider";
 import { cn } from "@/utils/classnames";
 import P4Logo from "@/assets/images/p4-icon.svg";
 import React from "react";
 import P4Modal from "./P4Channes-modal";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 type ChannelListProps = {
   channels: ChannelType[];
@@ -53,6 +54,21 @@ const ChannelList: React.FC<ChannelListProps> = ({ channels }) => {
   const p4Channels = channels.filter(
     (channel) => channel.name.startsWith("P4") && channel.name !== "P4 Plus"
   );
+
+  useEffect(() => {
+    const loadSelectedP4Channel = async () => {
+      const storedP4Channel = await AsyncStorage.getItem("selectedP4Channel");
+      if (storedP4Channel) {
+        setSelectedP4(JSON.parse(storedP4Channel));
+      }
+    };
+    loadSelectedP4Channel();
+  }, []);
+
+  const handleSelectP4 = async (channel: ChannelType) => {
+    setSelectedP4(channel);
+    await AsyncStorage.setItem("selectedP4Channel", JSON.stringify(channel));
+  };
 
   return (
     <View className="gap-4">
@@ -186,6 +202,7 @@ const ChannelList: React.FC<ChannelListProps> = ({ channels }) => {
         onClose={() => setModalVisible(false)}
         onSelectP4={(channel) => {
           setSelectedP4(channel);
+          handleSelectP4(channel);
           setModalVisible(false);
         }}
         p4Channels={p4Channels}
