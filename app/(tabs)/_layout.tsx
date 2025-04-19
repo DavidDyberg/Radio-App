00c172f50c9e4658c28f7c0e8a2ed97a91e94f89
David Dyberg <david.dyberg@gmail.com>
2025-04-19 12:13:00 +0200

    fix: tabbar height for android

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,7 @@
 import { Tabs } from "expo-router";
 import { SizeOption, useSize } from "@/utils/SizeProvider";
 import { TvMinimalPlay, Settings } from "lucide-react-native";
+import { Platform } from "react-native";
 
 export default function TabsLayout() {
   const { appSize } = useSize();
@@ -25,11 +26,14 @@ export default function TabsLayout() {
       ? 36
       : 26;
 
+  const tabBarHeight = Platform.OS === "android" ? 70 : 70;
+
   return (
     <Tabs
       screenOptions={{
         headerStyle: { backgroundColor: "#f8fafc" },
         headerTitleStyle: { fontSize: titleFontSize },
+        tabBarStyle: { height: tabBarHeight },
         tabBarLabelStyle: { fontSize: tabBarFontSize },
       }}
     >
